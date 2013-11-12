9674664cc4b060a1075f5273685d9e60495520a1
laskoviymishka <laskoviymishka@gmail.com>
2013-11-13 01:48:59 +0300

    Account membership added

---
+++ Diplom/MongoRepository/Model/User.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Security;
+
+namespace MongoRepository.Model
+{
+    public class User : MembershipUser
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string _id { get; set; }
+    }
+}
