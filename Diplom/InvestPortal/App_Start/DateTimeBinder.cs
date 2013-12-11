ae8de5e78d1ffecf9dc92c9a6360b7beee9f6949
laskoviymishka <laskoviymishka@gmail.com>
2013-12-11 17:36:47 +0300

    Исправлена ошибка с культурой даты.
    Принимается сервером только дата в формате dd/mm/yyyy

---
+++ Diplom/InvestPortal/App_Start/DateTimeBinder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InvestPortal.App_Start
+{
+    public class DateTimeBinder : IModelBinder
+    {
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            var value = bindingContext.ValueProvider.GetValue(bindingContext.ModelName).RawValue as string[];
+
+            var date = DateTime.ParseExact(value[0], "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+            return date;
+        }
+    }
+}
