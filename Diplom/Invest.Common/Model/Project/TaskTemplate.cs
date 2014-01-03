af6d8a658c409677098587432bf023db29924c47
laskoviymishka <laskoviymishka@gmail.com>
2014-01-03 14:17:37 +0300

    Task Template CRUD

--- Diplom/Invest.Common/Model/Project/TaskTemplate.cs
+++ Diplom/Invest.Common/Model/Project/TaskTemplate.cs
@@ -6,10 +6,10 @@ namespace Invest.Common.Model.Project
     public class TaskTemplate : IMongoEntity
     {
         public string _id { get; set; }
-
         public string Title { get; set; }
         public TaskTypes Type { get; set; }
         public ProjectWorkflow.State Step { get; set; }
         public string Body { get; set; }
+        public AdditionalInfo Info { get; set; }
     }
 }
---
+++ Diplom/InvestPortal/Controllers/TaskTemplateController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Invest.Common;
+using Invest.Common.Model.Project;
+using MongoDB.Bson;
+
+namespace InvestPortal.Controllers
+{
+    public class TaskTemplateController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(RepositoryContext.Current.All<TaskTemplate>());
+        }
+
+        public ActionResult Details(string id)
+        {
+            return View(RepositoryContext.Current.GetOne<TaskTemplate>(t => t._id == id));
+        }
+
+        public ActionResult Create()
+        {
+            return View(new TaskTemplate { _id = ObjectId.GenerateNewId().ToString() });
+        }
+
+        [HttpPost]
+        public ActionResult Create(TaskTemplate template)
+        {
+            if (ModelState.IsValid)
+            {
+                RepositoryContext.Current.Add(template);
+                return RedirectToAction("Index");
+            }
+
+            return View(template);
+        }
+
+        public ActionResult Edit(string id)
+        {
+            return View(RepositoryContext.Current.GetOne<TaskTemplate>(t => t._id == id));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(TaskTemplate template)
+        {
+            if (ModelState.IsValid)
+            {
+                RepositoryContext.Current.Update(template);
+            }
+
+            return View(template);
+        }
+
+        public ActionResult Delete(string id)
+        {
+            RepositoryContext.Current.Delete<TaskTemplate>(t => t._id == id);
+            return RedirectToAction("Index");
+        }
+    }
+}
